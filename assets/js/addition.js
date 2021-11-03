006041d3cc0037482bb277de2b08fadb9b8dcdb5
HTTCH <59481375+HTTCH@users.noreply.github.com>
2021-11-03 20:14:17 +0300

    Add files via upload

---
+++ assets/js/addition.js
@@ -0,0 +1,72 @@
+
+var wwidth = $(window).innerWidth(); 
+
+var nowdate = new Date();
+var season = nowdate.getMonth();
+
+if(season=="11" || season==0 || season==1){
+  $(document).ready(function(){//Snow
+    if (wwidth>770) {
+      $(document).snowfall({
+        flakeCount: 15,
+        minSize: 4,
+        maxSize: 8,
+        round: true,
+        shadow: false,
+        minSpeed: 0.5,
+        maxSpeed: 1,
+      });
+    }if (wwidth>520) {
+      $(document).snowfall({
+        flakeCount: 10,
+        minSize: 4,
+        maxSize: 8,
+        round: true,
+        shadow: false,
+        minSpeed: 0.5,
+        maxSpeed: 1,
+      });
+    }else{
+      $(document).snowfall({
+        flakeCount: 7,
+        minSize: 4,
+        maxSize: 8,
+        round: true,
+        shadow: false,
+        minSpeed: 0.5,
+        maxSpeed: 1,
+      });
+    }//site elements
+    $('body').css('background', '#eaeef563');
+    $('footer').css('background', '#f5f3fd');
+    $('.copyright').css('background', 'rgb(202 209 222)');
+    $('#header').css('background', 'rgba(202, 209, 222, 0.95)');
+    $('.nav').css('background', 'rgba(202, 209, 222, 0.95)');
+    $('.nav').css('border-color', 'rgba(187, 194, 206, 0.85)');
+    //buttons
+    $(".btn").addClass("winter");
+    $(".trigger").attr('id', 'btn__modal-winter');
+    //images
+    $('.intro').css('background', 'url("../assets/images/intro-winter.png") center no-repeat');
+    $('.intro').css('background-size', 'cover');
+
+
+  });
+}if(season=="8" || season==9 || season==10){
+    $('body').css('background', 'rgba(245, 241, 234, 0.39);');
+    $('footer').css('background', 'rgb(253 243 243)');
+    $('.copyright').css('background', 'rgb(222 202 202)');
+    $('#header').css('background', 'rgba(227, 130, 0, 0.95)');
+    $('.nav').css('background', 'rgba(227, 130, 0, 0.95)');
+    $('.nav').css('border-color', 'rgba(212, 121, 0, 0.9)');
+    //buttons
+    $(".btn").addClass("autumn");
+    $(".trigger").attr('id', 'btn__modal-autumn');
+    //images
+    $('.intro').css('background', 'url("../assets/images/intro-autumn.png") center no-repeat');
+    $('.intro').css('background-size', 'cover');
+}else{
+    $('.intro').css('background', 'url("../assets/images/intro.png") center no-repeat');
+    $('.intro').css('background-size', 'cover');
+}
+
